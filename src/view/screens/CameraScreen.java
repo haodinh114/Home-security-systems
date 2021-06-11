6b19e14d56ff52f887f9eabeaf2a4765eeb8eb31
haodinh114 <minhhao114@gmail.com>
2021-06-11 00:39:28 -0700

    Temperature Display

--- src/view/screens/CameraScreen.java
+++ src/view/screens/CameraScreen.java
@@ -2,10 +2,6 @@ package view.screens;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class CameraScreen extends JPanel {
 
--- src/view/screens/TemperatureScreen.java
+++ src/view/screens/TemperatureScreen.java
@@ -1,10 +1,27 @@
 package view.screens;
 
-import javax.swing.*;
+import model.SystemController;
+import view.Main;
 
-public class TemperatureScreen extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class TemperatureScreen extends JPanel implements Refreshable{
     private JTextArea screen;
     public TemperatureScreen() {
 
+        this.setLayout(new BorderLayout());
+
+        screen = new JTextArea();
+        screen.setEditable(false);
+        add(new JScrollPane(screen), BorderLayout.CENTER);
+        refresh();
     }
+
+    @Override
+    public void refresh() {
+        SystemController controller = Main.MAIN_SYSTEM;
+        screen.setText(controller.getTemperature());
+    }
+
 }
