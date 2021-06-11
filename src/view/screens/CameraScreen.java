c58bc5b0042c816c7dfef70a0c529a9a7236e827
Embert Pezzali <pezzalie@uw.edu>
2021-06-11 12:59:10 -0700

    Speed up Scrolling of CameraScreen

--- src/view/screens/CameraScreen.java
+++ src/view/screens/CameraScreen.java
@@ -1,6 +1,5 @@
 package view.screens;
 
-import view.Border;
 import view.Main;
 
 import javax.swing.*;
@@ -8,7 +7,7 @@ import java.awt.*;
 
 public class CameraScreen extends JPanel {
 
-    Picture frontdoor, bedroom, livingroom, kitchen;
+    Picture frontDoor, bedroom, livingRoom, kitchen;
 
     /**
      * Creates the
@@ -16,24 +15,28 @@ public class CameraScreen extends JPanel {
     public CameraScreen() {
         this.setLayout(new BorderLayout());
         // gets the files and make them pictures
-        frontdoor = new Picture("src/assets/frontdoor.png");
+        frontDoor = new Picture("src/assets/frontdoor.png");
         bedroom = new Picture("src/assets/bedroom.png");
-        livingroom = new Picture("src/assets/livingroom.png");
+        livingRoom = new Picture("src/assets/livingroom.png");
         kitchen = new Picture("src/assets/kitchen.png");
         // makes all of the pictures
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(frontdoor);
+        panel.add(frontDoor);
         panel.add(bedroom);
-        panel.add(livingroom);
+        panel.add(livingRoom);
         panel.add(kitchen);
-        // show picture
-        add(new JScrollPane(panel));
+        // put in scroll pane
+        JScrollPane pane = new JScrollPane(panel);
+        pane.getVerticalScrollBar().setUnitIncrement(10);
+
+        // add scroll pane
+        add(pane);
     }
 
     // private class to have resizable pictures
-    private class Picture extends JPanel{
-        private ImageIcon icon;
+    private static class Picture extends JPanel{
+        private final ImageIcon icon;
 
         // creates a picture with a default size based on the default screen size
         private Picture(String s) {
